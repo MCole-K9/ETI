9b8ebae1f4df0019866ac495fd686afd9c736cf7
Matthew Cole <m.b.cole300@gmail.com>
2022-11-27 15:23:38 -0500

    Template System Setup (Experimental)

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+  include 'base.php';
+?>
+
+<?php startblock("head");?>
+    <title>Home</title>
+<?php endblock();?>
+
+<?php startblock("content");?>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+<?php endblock();?>
