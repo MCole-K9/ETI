95ed65727c9d9967bbe968cbd18d2046dfbf80bd
Matthew Cole <m.b.cole300@gmail.com>
2022-12-07 14:37:58 -0500

    Homonyms Intro

---
+++ homonyms.php
@@ -0,0 +1,31 @@
+<?php
+    include 'base.php';
+?>
+
+
+<?php startblock("head"); ?>
+    <title>Learn | Homonyms</title>
+<?php endblock(); ?>
+
+<?php startblock("content"); ?>
+
+<section id="header">
+    <div class="mx-auto text-center shadow my-5 w-75 rounded">
+        <h1 class="py-3">Homonyms</h1>
+    </div>
+</section>
+
+<section id="intro" class="row mt-5">
+    <div class="col-12 col-lg-6 ">
+        <h5 class="mb-3">What are Homonyms?</h5>
+        <p class="w-75 lh-lg my-4">Homonyms are words that are spelled the same but have different meanings. For example, the word "read" can be a verb or a noun. It can mean "to read" or "a reading".</p>
+    </div>
+    <div class="col-12 col-lg-6">
+        <div class=""class="ratio ratio-16x9 py-5">
+            <iframe class="w-100" src="https://www.youtube.com/embed/iUVrp11MCeE" style="height: 300px ;"></iframe>
+        </div> 
+    </div>
+
+</section>
+
+<?php endblock()?>
