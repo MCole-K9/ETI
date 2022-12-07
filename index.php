2aa38fc7aaedbd0f777eeda592938a59be2a8dc2
Matthew Cole <m.b.cole300@gmail.com>
2022-12-07 11:41:23 -0500

    Draft of HomePage

--- index.php
+++ index.php
@@ -1,12 +1,67 @@
-<?php 
-  include 'base.php';
+<?php
+include 'base.php';
 ?>
 
-<?php startblock("head");?>
-    <title>Home</title>
-<?php endblock();?>
+<?php startblock("head"); ?>
+<title>Home</title>
+<?php endblock(); ?>
 
-<?php startblock("content");?>
-    <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
-<?php endblock();?>
+<?php startblock("content"); ?>
+<section id="hero" class="row mt-5">
+  <div class="col-12 col-lg-6">
+    <h1 class="lh-base ">Having Trouble<br>With English? </h1>
+    <h3>Look No Further</h2>
+    <h5 class="lh-base">Welcome to English Training<br>Interactive</h5>
+  </div>
+  <div class="col-12 col-lg-6">
+    <div class="placeholder w-100 h-100"></div>
+    <!-- video / ad goes here -->
+  </div>
+</section>
+
+
+<section class="row mt-5">
+  <div class="col-12 col-lg-6">
+    <h5 class="mb-3">Some Heading</h5>
+    <p class="w-75">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quod odit perferendis molestias sed similique ullam fugiat voluptatibus soluta, eum nisi veniam blanditiis obcaecati in repellendus cupiditate, quaerat eos minima.</p>
+
+
+    </div>
+  </div>
+  <div class="col-12 col-lg-6">
+    <h5 class="mb-3">Some of Our Topics</h5>
+    <ul class="list-group">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Homonyms
+        <span class="badge bg-primary rounded-pill">
+          <a class="text-white text-decoration-none" href="homonyms.php">Go <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+            </svg>
+          </a>
+        </span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Homophones
+        <span class="badge bg-primary rounded-pill">
+          <a class="text-white text-decoration-none" href="">Go <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+            </svg>
+          </a>
+        </span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Homographs
+        <span class="badge bg-primary rounded-pill">
+          <a class="text-white text-decoration-none" href="">Go <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+            </svg>
+          </a>
+        </span>
+      </li>
+    </ul>
+  </div>
+
+</section>
+
+
+<?php endblock(); ?>
