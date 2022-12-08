98ca409487471fe0620a786c080856beaf3b9285
Matthew Cole <m.b.cole300@gmail.com>
2022-12-08 13:27:28 -0500

    formatted document ;)

--- homonyms.php
+++ homonyms.php
@@ -21,29 +21,29 @@
         <p class="w-75 lh-lg my-4">Homonyms are words that are spelled the same but have different meanings. For example, the word "read" can be a verb or a noun. It can mean "to read" or "a reading".</p>
     </div>
     <div class="col-12 col-lg-6">
-        <div class=""class="ratio ratio-16x9 py-5">
+        <div class="" class="ratio ratio-16x9 py-5">
             <iframe class="w-100" src="https://www.youtube.com/embed/iUVrp11MCeE" style="height: 300px ;"></iframe>
-        </div> 
+        </div>
     </div>
 
 </section>
 <section id="activity-first" class="mt-5">
-    <div class="border border-success rounded p-3"> 
-    <p class="bg-success rounded p-2 text-white font-bold" style="margin-top: -32px; width: fit-content">Activity</p>
-    <p class=""><strong>Identify the Homonyms by the pair of definitions below</strong></p>
-    <div class="mb-3 row justify-content-between">
-    <label for="" class="col-sm-5 col-form-label">A Holding area for animals</label>
-        <div class="col-sm-5">
-        <input type="text" class="form-control" id="">
+    <div class="border border-success rounded p-3">
+        <p class="bg-success rounded p-2 text-white font-bold" style="margin-top: -32px; width: fit-content">Activity</p>
+        <p class=""><strong>Identify the Homonyms by the pair of definitions below</strong></p>
+        <div class="mb-3 row justify-content-between">
+            <label for="" class="col-sm-5 col-form-label">A Holding area for animals</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" id="">
+            </div>
         </div>
-    </div>
-    <div class="mb-3 row justify-content-between">
-        <label for="" class="col-sm-5 col-form-label">A writing instrument</label>
-        <div class="col-sm-5">
-        <input type="text" class="form-control" id="">
+        <div class="mb-3 row justify-content-between">
+            <label for="" class="col-sm-5 col-form-label">A writing instrument</label>
+            <div class="col-sm-5">
+                <input type="text" class="form-control" id="">
+            </div>
         </div>
-    </div>
     </div>
 </section>
 
-<?php endblock()?>
+<?php endblock() ?>
