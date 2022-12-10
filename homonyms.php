b7076553a1d8a7ee18bbdc20e803c992ca0eeada
Matthew Cole <m.b.cole300@gmail.com>
2022-12-10 17:01:51 -0500

    homophones page

--- homonyms.php
+++ homonyms.php
@@ -16,11 +16,11 @@ include 'base.php';
 </section>
 
 <section id="intro" class="row mt-5">
-    <div class="col-12 col-lg-6 ">
+    <div class="col-12 col-lg-5 ">
         <h5 class="mb-3">What are Homonyms?</h5>
         <p class="w-75 lh-lg my-4">Homonyms are words that are spelled the same but have different meanings. For example, the word "read" can be a verb or a noun. It can mean "to read" or "a reading".</p>
     </div>
-    <div class="col-12 col-lg-6">
+    <div class="col-12 col-lg-7">
         <div class="" class="ratio ratio-16x9 py-5">
             <iframe class="w-100" src="https://www.youtube.com/embed/iUVrp11MCeE" style="height: 300px ;"></iframe>
         </div>
---
+++ homophones.php
@@ -0,0 +1,79 @@
+<?php
+include 'base.php';
+?>
+
+
+<?php startblock("head"); ?>
+<title>Learn | Homonyms</title>
+<?php endblock(); ?>
+
+<?php startblock("content"); ?>
+
+<section id="header">
+    <div class="mx-auto text-center shadow my-5 w-75 rounded">
+        <h2 class="py-3">Homophones</h2>
+    </div>
+</section>
+
+<section id="intro" class="row mt-5">
+    <div class="col-12 col-lg-5 ">
+        <h5 class="mb-3">What are Homophones?</h5>
+        <p class="w-75 lh-lg my-4">Homophones are words that have the same sound, in terms of how they are pronounced but have a different meaning and are (often) spelt differently.</p>
+    </div>
+    <div class="col-12 col-lg-7">
+        <div class="" class="ratio ratio-16x9 py-5">
+            <iframe class="w-100" src="https://www.youtube.com/embed/wm5LwCcwRQ8" style="height: 300px ;"></iframe>
+        </div>
+    </div>
+
+</section>
+
+<section id="examples" class="my-5">
+    <h3>Homophone Examples</h3>
+    <table class="table mb-5 table-hover">
+    <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Word A</th>
+                <th scope="col">Word B</th>
+                <th scope="col">Word A. Meaning</th>
+                <th scope="col">Word B. Meaning</th>
+            </tr>
+        </thead>
+        <tbody >
+            <tr>
+                <th scope="row">1</th>
+                <td class="fw-bold">Weather</td>
+                <td class="fw-bold">Whether</td>
+                <td>'Weather' is a noun that refers to the current atmospheric conditions, such as if it's windy, rainy, or sunny.</td>
+                <td>'Whether' is neither a noun nor a verb - it's a conjunction. It joins two words or phrases together in a sentence. It's used similarly to the word 'if'. </td>
+            </tr>
+        
+            <tr>
+                <th scope="row">2</th>
+                <td class="fw-bold">Night</td>
+                <td class="fw-bold">Knight</td>
+                <td> Night is defined as “the period between sunset and sunrise, especially the hours of darkness. - Opposite of day</td>
+                <td>A knight is a man who served in the military during medieval times as a high-ranking soldier.  </td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td class="fw-bold">Here</td>
+                <td class="fw-bold">Hear</td>
+                <td>The word here means, "in, at or to this place". Simply speaking, here is used when you are referring to the place where you are. It is an adverb, a word that gives more information about a verb or adjective.</td>
+                <td> Hear is a verb, meaning to listen to a sound. It can also mean gaining a message or information.</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td class="fw-bold">Groan</td>
+                <td class="fw-bold">Grown</td>
+                <td>Indicate pain, discomfort, or displeasure</td>
+                <td>(of animals, as well as humans) fully developed</td>
+            </tr>
+        
+        </tbody>
+    </table>
+</section>
+
+
+<?php endblock() ?>
