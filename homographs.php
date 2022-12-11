ec1e193d0c692ceb643736a4bbd1b09832b759c2
Matthew Cole <m.b.cole300@gmail.com>
2022-12-11 14:43:11 -0500

    Homographs page

---
+++ homographs.php
@@ -0,0 +1,84 @@
+<?php
+include 'base.php';
+?>
+
+
+<?php startblock("head"); ?>
+<title>Learn | Homonyms</title>
+<?php endblock(); ?>
+
+<?php startblock("content"); ?>
+
+<section id="header">
+    <div class="mx-auto text-center shadow my-5 w-75 rounded">
+        <h2 class="py-3">Homographs</h2>
+    </div>
+</section>
+
+<section id="intro" class="row mt-5">
+    <div class="col-12 col-lg-5 ">
+        <h5 class="mb-3">What are Homographs?</h5>
+        <p class="w-75 lh-lg my-4">Homographs  are words that have the same spelling but differ in origin, meaning, and sometimes pronunciation</p>
+    </div>
+    <div class="col-12 col-lg-7">
+        <div class="" class="ratio ratio-16x9 py-5">
+            <iframe class="w-100" src="https://www.youtube.com/embed/FUQKaUncNmQ" style="height: 300px ;"></iframe>
+        </div>
+    </div>
+
+</section>
+
+<section id="examples" class="my-5">
+    <h3>Homograph Examples</h3>
+    <table class="table mb-5 table-hover">
+    <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Word </th>
+                <th scope="col">Meaning A.</th>
+                <th scope="col">Meaning B.</th>
+            </tr>
+        </thead>
+        <tbody >
+            <tr>
+                <th scope="row">1</th>
+                <td class="fw-bold">Bear</td>
+                <td>To carry or endure</td>
+                <td>An animal with a shaggy coat </td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td class="fw-bold">Attribute</td>
+                <td>to think of as belonging to or originating in some person, place</td>
+                <td> a characteristic or quality</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td class="fw-bold">Contract</td>
+                <td>a written agreement</td>
+                <td>to get, acquire or incur</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td class="fw-bold">Evening</td>
+                <td>late afternoon</td>
+                <td>making more even</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td class="fw-bold">Tear</td>
+                <td>to rip</td>
+                <td>a drop of water from the eye</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td class="fw-bold">Second</td>
+                <td>1/60 of a minute</td>
+                <td>the position after first</td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+
+
+<?php endblock() ?>
