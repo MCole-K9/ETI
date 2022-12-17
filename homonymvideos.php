c39e44f67eb251431bc847936e5975abc93553a3
khalid sheriff <khalalimsheriff@gmail.com>
2022-12-16 20:59:36 -0500

    added videos to videos page

---
+++ homonymvideos.php
@@ -0,0 +1,23 @@
+<?php include "base.php" ?>
+
+<?php startBlock("head") ?>
+    <title>Educational Videos on Homonyms</title>
+<?php endBlock("head") ?>
+
+<?php startBlock("content") ?>
+    <section class="mt-6">
+        <div class="text-center mt-6">
+            <h2>Educational Videos on Homonyms</h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/iUVrp11MCeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        
+        <div class="text-center mt-3">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/j2lB--AHV-I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <div class="text-center mt-3">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/aICo2wXaPQE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </section>   
+
+<?php endBlock ("content") ?>
