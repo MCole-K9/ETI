b11db13070ebe9a6eb2e24a2c59dbace2414c4f3
khalid sheriff <khalalimsheriff@gmail.com>
2022-12-16 21:33:53 -0500

    various changes across pages to improve appearance; added contact info, videos, logo on about

--- homonymvideos.php
+++ homonymvideos.php
@@ -4,18 +4,18 @@
     <title>Educational Videos on Homonyms</title>
 <?php endBlock("head") ?>
 
-<?php startBlock("content") ?>
-    <section class="mt-6">
-        <div class="text-center mt-6">
+<?php startBlock("content"); ?>
+    <section class="mt-5">
+        <div class="mx-auto text-center">
             <h2>Educational Videos on Homonyms</h2>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/iUVrp11MCeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         
-        <div class="text-center mt-3">
+        <div class="mx-auto mt-3 text-center">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/j2lB--AHV-I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
-        <div class="text-center mt-3">
+        <div class="mx-auto mt-3 text-center">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/aICo2wXaPQE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </section>   
