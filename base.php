f2dc733569b261fd1ea0b3935b9d8abb964df975
khalid sheriff <khalalimsheriff@gmail.com>
2022-12-16 19:56:43 -0500

    minor adjustments; trying to remember boostrap syntax

--- base.php
+++ base.php
@@ -14,7 +14,7 @@
 <body>
     <nav id="etiNav" class="navbar navbar-expand-lg shadow">
     <div class="container">
-        <a class="navbar-brand" href="index.php"><img id="navbarLogo"src="media/eti_grey.svg" alt="ETI Logo"></svg> English Training Institute</a>
+        <a class="navbar-brand logo-text" href="index.php"><img id="navbarLogo"src="media/eti_grey.svg" alt="ETI Logo"></svg> English Training Interactive</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -60,6 +60,18 @@
 
  .navbar {
     background-color: #4A53FC;
+ }
+
+ .nav-link {
+    color: #DBE4EE;
+ }
+
+ a[class~="nav-link active"] {
+    color: #F8FAFC;
+ }
+
+ .logo-text {
+    color: #DBE4EE;
  }
 
  #navbarLogo {
